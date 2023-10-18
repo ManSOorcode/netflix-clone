acfe96a02c7eec2cb143f8bf8c1cce74577e4a77
Mansoor Khan <mansoorKh1098@gmail.com>
2023-10-18 22:34:16 +0530

    ✨ feat: nav bar is completed

---
+++ src/component/Nav.jsx
@@ -0,0 +1,42 @@
+// import React from "react";
+import { useState } from "react";
+import "./Nav.css";
+import { useEffect } from "react";
+
+const Nav = () => {
+  const [isScroll, setScrollStyle] = useState(false);
+
+  const settingScrollStyle = () => {
+    if (window.scrollY > 100) {
+      setScrollStyle(true);
+    } else {
+      setScrollStyle(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", settingScrollStyle);
+
+    return () => document.removeEventListener("scroll", settingScrollStyle);
+  }, []);
+  return (
+    <div className={`nav ${isScroll && "nav_color"}`}>
+      <div className="nav_container">
+        <img
+          className="nav_logo"
+          // src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt="netflix-logo"
+        />
+
+        <img
+          className="nav_avatar"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png"
+          alt="netflix-avatar"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
