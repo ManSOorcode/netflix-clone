f94b9b3f84cc98e5a9613d147e38f9c72fdab021
Mansoor Khan <mansoorKh1098@gmail.com>
2023-10-20 01:30:49 +0530

    🚧 feat: data is fecthing set to state

---
+++ src/component/Banner.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import axios from "../api/axios";
+
+import requests from "../api/Request";
+
+const Banner = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+
+      setMovies(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+
+      return request;
+    }
+
+    fetchData();
+
+    return () => abortController.abort();
+  }, []);
+
+  const descriptionModification = (description, value) => {
+    console.log(description?.length);
+    return description?.length > value
+      ? `${description?.substr(0, 100 - 1)}...`
+      : description;
+  };
+
+  // console.log(movies);
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movies?.backdrop_path}")`,
+        backgroundPosition: "center center",
+        objectFit: "contain",
+        // backgroundAttachment: "fixed",
+      }}
+    >
+      <div className="banner_content">
+        <h1 className="banner_title">{movies?.name}</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">
+          {descriptionModification(movies?.overview, 100)}
+        </h1>
+      </div>
+      <div className="banner_fadeBottom" />
+    </header>
+  );
+};
+
+export default Banner;
