a09096f058390635bd8a407d8e46ae6f09d19314
Mansoor Khan <mansoorKh1098@gmail.com>
2023-10-27 01:29:11 +0530

    📝 feat: login and sigup form is created functinality remain

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginScreen.css";
+import SignUpForm from "./SignUpForm";
 
 const LoginScreen = () => {
+  const [singUp, setSignUp] = useState(false);
+
   return (
     <div className="screen">
       <div className="screen_background">
@@ -10,8 +13,33 @@ const LoginScreen = () => {
           className="screen_logo"
           alt=""
         />
-        <button className="screen_button">Sign In</button>
+        <button className="screen_button" onClick={() => setSignUp(true)}>
+          Sign In
+        </button>
         <div className="screen_gradient"></div>
+      </div>
+
+      <div className="screen_body">
+        {singUp ? (
+          <SignUpForm />
+        ) : (
+          <>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+            <div className="screen_input">
+              <form onClick={() => setSignUp(true)}>
+                <div className="screen_center">
+                  <input type="emai" placeholder="Email Address" />
+                  <button className="screen_getStarted">GET STARTED</button>
+                </div>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
---
+++ src/screens/SignUpForm.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./SignUpform.css";
+
+const SignUpForm = () => {
+  return (
+    <div className="signUpScreen">
+      <form>
+        <h1>Sign In</h1>
+        {/* <div className="signupScreen_body"> */}
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        {/* </div> */}
+        <button type="submit">Sign In</button>
+        <h4>
+          <span className="signupScreen_gray">New to Netflix?</span>
+          <span className="signupScreen_link">Sign up now.</span>
+        </h4>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
