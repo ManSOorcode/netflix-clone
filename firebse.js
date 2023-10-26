a65307e6d8c969649afa6266251b0c08426289fb
Mansoor Khan <mansoorKh1098@gmail.com>
2023-10-27 03:29:32 +0530

    ✨ feat: updated version of firebase auth

--- firebse.js
+++ firebse.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAEFgOq6ZxftdQ4rN_Al29GcdujnlBhRqY",
   authDomain: "netflix-clone-9f049.firebaseapp.com",
@@ -6,3 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "495493944459",
   appId: "1:495493944459:web:7bb5fe399222de26fcb08e",
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+export { auth, db };
