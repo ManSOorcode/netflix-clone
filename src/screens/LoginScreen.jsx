30f2c07f5784a8ecceb6f923f8a9efe0ac97f6cc
Mansoor Khan <mansoorKh1098@gmail.com>
2023-11-04 16:09:55 +0530

    🔥 feat: now login Screen is responsive

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -16,30 +16,30 @@ const LoginScreen = () => {
         <button className="screen_button" onClick={() => setSignUp(true)}>
           Sign In
         </button>
-        <div className="screen_gradient"></div>
-      </div>
-
-      <div className="screen_body">
-        {singUp ? (
-          <SignUpForm />
-        ) : (
-          <>
-            <h1>Unlimited films, TV programmes and more.</h1>
-            <h2>Watch anywhere. Cancel at any time.</h2>
-            <h3>
-              Ready to watch? Enter your email to create or restart your
-              membership.
-            </h3>
-            <div className="screen_input">
-              <form onClick={() => setSignUp(true)}>
-                <div className="screen_center">
-                  <input type="emai" placeholder="Email Address" />
-                  <button className="screen_getStarted">GET STARTED</button>
+        <div className="screen_gradient">
+          <div className="screen_body">
+            {singUp ? (
+              <SignUpForm />
+            ) : (
+              <>
+                <h1>Unlimited films, TV programmes and more.</h1>
+                <h2>Watch anywhere. Cancel at any time.</h2>
+                <h3>
+                  Ready to watch? Enter your email to create or restart your
+                  membership.
+                </h3>
+                <div className="screen_input">
+                  <form onClick={() => setSignUp(true)}>
+                    <div className="screen_center">
+                      <input type="emai" placeholder="Email Address" />
+                      <button className="screen_getStarted">GET STARTED</button>
+                    </div>
+                  </form>
                 </div>
-              </form>
-            </div>
-          </>
-        )}
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
